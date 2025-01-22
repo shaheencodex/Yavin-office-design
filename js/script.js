faae1538b54d8962da5fbc76eeec679c08f2a179
Shaheen Salma <shaheencodex@gmail.com>
2025-01-22 15:50:04 +0530

    back To TopButton added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 function userScroll() {
     const navbar = document.querySelector('.navbar');
+    const toTopBtn = document.querySelector('#to-top');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             navbar.classList.add('navbar-sticky');   
+            toTopBtn.classList.add('show');
         }
         else {
             navbar.classList.remove('navbar-sticky');
+            toTopBtn.classList.remove('show');
         }
     });
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -33,5 +41,8 @@ function incrementStats() {
         updateCounter();
     });
 }
+
+// Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
